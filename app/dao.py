e4eff778f29795571bb8aa099c2fef5283d19fc4
maivo0902@gmai.com <maivo0902@gmai.com>
2024-11-06 16:28:32 +0700

    update

---
+++ app/dao.py
@@ -0,0 +1,10 @@
+from app.models import Category, Product
+
+def load_categories():
+    return Category.query.order_by('id').all()
+
+
+def load_products():
+    query = Product.query
+    #...
+    return query.all()
